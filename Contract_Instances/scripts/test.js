0ed43ded9e79de929f7ebd01595e05cb62869910
Juan Felipe <juan_fe_7@live.com>
2021-11-06 18:12:19 -0500

    Testing script for hardhat's local blockchain

---
+++ Contract_Instances/scripts/test.js
@@ -0,0 +1,33 @@
+const hre = require('hardhat');
+
+async function main() {
+  const [owner, randomPerson] = await hre.ethers.getSigners();
+
+  const VoteTest = await hre.ethers.getContractFactory('VoteTest');
+  const vote = await VoteTest.deploy();
+  await vote.deployed();
+
+  const voteSent = await vote.castVote(owner.address);
+  await voteSent.wait();
+
+  const anotherVote = await vote.castVote(randomPerson.address);
+  await anotherVote.wait();
+
+  const hasVoted = await vote.checkVote(owner.address);
+  console.log({
+    voteStatus: {
+      hasVoted: hasVoted[0],
+      timestamp: new Date(hasVoted[1] * 1000),
+    },
+  });
+
+  const total = await vote.checkTotalVotes();
+  console.log({ total: total.toString() });
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
